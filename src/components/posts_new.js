94425acacfc9a5ee11bbe0464500f8c705e83ed3
Miguel <miguel@semanticrobot.com>
2018-02-03 10:21:42 -0300

    First Commit

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render(){
+      return(
+        <div>
+          POsts..!
+        </div>
+      );
+  }
+}
+
+export default PostsNew;
